c53de5fff74d635e2fe4354e8ac8533c4e6e8f33
spiroskonstantinidis <60156291+spiros-k@users.noreply.github.com>
2024-05-23 21:03:24 +0300

    create days counter

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
 let date = new Date();
-const timeArray = ["Hours", "Minutes", "Seconds"];
+const timeArray = ["Days", "Hours", "Minutes", "Seconds"];
 
-// let currentMonth = date.getMonth();
-//let currentDay = date.getDate();
+
 let currentHour = date.getHours();
 let currentMinute = date.getMinutes();
 let currentSecond = date.getSeconds();
@@ -11,27 +10,22 @@ let currentSecond = date.getSeconds();
 let currentDate = [`${currentHour}`,  `${currentMinute}`,  `${currentSecond}`];
 let countdownDate = new Date("June 02, 2024 23:59:59");
 
-// TRIED USING GETTIME()
-// let currentTime = date.getTime();
-// let countdownTime = countdownDate.getTime()
-// console.log(currentTime, countdownTime)
-// console.log(countdownTime - currentTime)
 
-// let countdownMonth = countdownDate.getMonth();
-//let countdownDay = countdownDate.getDate()
 let countdownHour = countdownDate.getHours();
-let countdownMinute = countdownDate.getMinutes()
-let countdownSecond = countdownDate.getSeconds()
+let countdownMinute = countdownDate.getMinutes();
+let countdownSecond = countdownDate.getSeconds();
 
 countdownDate = [`${countdownHour}`,  `${countdownMinute}`,  `${countdownSecond}`];
 
-console.log(currentDate)
-console.log(countdownDate)
+
 let output = [];
 for(let i = 0; i < currentDate.length; i++){
     output.push(countdownDate[i] - currentDate[i])
     console.log(output)
 }
+
+// CHECK WHAT THE CURRENT MONTH IS AND DEPENDING ON THE DATE
+// -COUNTS THE REMAINING DAYS FOR THIS MONTH TO PASS
 let countdownMonth
 let monthOfYear = date.getMonth() + 1;
 console.log(monthOfYear)
@@ -44,30 +38,50 @@ if(monthOfYear === 1){
 } else if(monthOfYear === 3){
     countdownMonth = 31 - monthNum;
 } else if(monthOfYear === 4){
-
+    countdownMonth = 30 - monthNum;
+}else if(monthOfYear === 5){
+    countdownMonth = 31 - monthNum;
+} else if(monthOfYear === 6){
+    countdownMonth = 30 - monthNum;
+} else if(monthOfYear === 7){
+    countdownMonth = 31 - monthNum;
+} else if(monthOfYear === 8){
+    countdownMonth = 31 - monthNum;
+}else if(monthOfYear === 9){
+    countdownMonth = 30 - monthNum;
+} else if(monthOfYear === 10){
+    countdownMonth = 31 - monthNum;
+} else if(monthOfYear === 11){
+    countdownMonth = 30 - monthNum;
+} else if(monthOfYear === 12){
+    countdownMonth = 31 - monthNum;
+}else {
+    console.log("Something Went Wrong / No such a month")
 }
 
+countdownMonth = countdownMonth + (new Date("June 02, 2024 23:59:59").getDate());
+console.log("Hey")
+console.log(countdownMonth)
 
-//  1. CORRECT TIME SHOWN
-//  2. MONTHS, DAYS... SECONDS text SHOW
-//  3. UPDATE EVERY SECOND
+output.unshift(countdownMonth)
+console.log(output)
 
-
+// WHAT TO SHOW WHEN THE PAGE LOADS
 document.addEventListener("DOMContentLoaded", () => {
     
-    let time = output.map((item) => {
+    let time = output.map((item) => {                                    // FOR EACH ITEM A FUNC RUNS AND CREATES A NEW DIV
        
         return (`<div id="countdown-div">
             <h1 id="countdown-number">${item}</h1>
             <p class="countdown-text"></p>
             </div>`)
     });
-    let timeDiv = time.join("")
-    console.log(timeDiv)
-    document.getElementById("countdown-section").innerHTML = timeDiv;
-    for(let i = 0; i < timeArray.length; i++){
+    let timeDiv = time.join("")                                          // JOIN THE WHOLE HTML THAT CREATED ABOVE WITH SPACE
+    document.getElementById("countdown-section").innerHTML = timeDiv;    //SHOW THAT HTML
+    for(let i = 0; i < timeArray.length; i++){                           //SHOW THE NAMES (DAYS, HOURS..) REMAINING
         document.getElementsByClassName("countdown-text")[i].textContent = timeArray[i];
     }
 })
 
-
+// 1. UPDATE EVERY SECOND WITHOUT THE USER RELOADING THE PAGE
+// 2. WHEN THE COUNTDOWN ZEROS SHOULD SHOW SOMETHING AND STOP THE COUNTDOWN
